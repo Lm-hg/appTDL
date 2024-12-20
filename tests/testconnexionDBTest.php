16f3c045bf4d85b52eac05e3e51af4676c4271c8
Lm-hg <you@example.com>
2024-12-20 14:39:59 +0100

    mon app

---
+++ tests/testconnexionDBTest.php
@@ -0,0 +1,16 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class DatabaseConnectionTest extends TestCase {
+
+    public function testconnexionReussie() {
+        $host = 'mysql';
+        $user = 'root';
+        $password = 'root';
+        $database = 'init';
+        require_once '../php/connect.php';
+        $db = connectDatabase($host, $user, $password, $database);
+        $this->assertNotFalse($db, "La connexion n'a pas échoué comme attendu");
+    }
+
+}
